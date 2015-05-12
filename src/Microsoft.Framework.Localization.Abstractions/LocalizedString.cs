78f2a99d046acd362c8a86dcf57a97f5fa24cdb6
Eilon Lipton <Eilon@users.noreply.github.com>
2015-05-11 22:29:27 -0700

    Update LocalizedString.cs

--- src/Microsoft.Framework.Localization.Abstractions/LocalizedString.cs
+++ src/Microsoft.Framework.Localization.Abstractions/LocalizedString.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
 
 using Microsoft.Framework.Internal;
-
+// 안뇽
 namespace Microsoft.Framework.Localization
 {
     /// <summary>
