1d6b63a019579f7707a13942656005c4a5c77328
damianedwards <damian@damianedwards.com>
2015-05-19 13:26:02 -0700

    Added a unit test for CultureInfo tree walk caching - #15

---
+++ test/Microsoft.Framework.Localization.Test/ResourceManagerStringLocalizerTest.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved. 
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Framework.Localization.Test
+{
+    public class ResourceManagerStringLocalizerTest
+    {
+        [Fact]
+        public void EnumeratorCachesCultureWalk()
+        {
+            // Arrange
+            var resourceManager = new Mock<ResourceManager>();
+            var resourceAssembly = new Mock<TestAssembly>();
+            resourceAssembly.Setup(rm => rm.GetManifestResourceStream(It.IsAny<string>()))
+                .Returns(() => MakeResourceStream());
+            var baseName = "test";
+            var localizer = new ResourceManagerStringLocalizer(
+                resourceManager.Object,
+                resourceAssembly.Object,
+                baseName);
+
+            // Act
+            for (int i = 0; i < 5; i++)
+            {
+                localizer.ToList();
+            }
+
+            // Assert
+            var expectedCallCount = GetCultureInfoDepth(CultureInfo.CurrentUICulture);
+            resourceAssembly.Verify(
+                rm => rm.GetManifestResourceStream(It.IsAny<string>()),
+                Times.Exactly(expectedCallCount));
+        }
+
+        private static Stream MakeResourceStream()
+        {
+            var stream = new MemoryStream();
+            var resourceWriter = new ResourceWriter(stream);            
+            resourceWriter.AddResource("TestName", "value");
+            resourceWriter.Generate();
+            stream.Position = 0;
+            return stream;
+        }
+
+        private static int GetCultureInfoDepth(CultureInfo culture)
+        {
+            var result = 0;
+            var currentCulture = culture;
+
+            while (true)
+            {
+                result++;
+
+                if (currentCulture == currentCulture.Parent)
+                {
+                    break;
+                }
+
+                currentCulture = currentCulture.Parent;
+            }
+
+            return result;
+        }
+
+        public class TestAssembly : Assembly
+        {
+
+        }
+    }
+}
