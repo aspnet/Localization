6b84e59dd6aac97d57695d9024875878afd277e0
Hisham Abdullah Bin Ateya <hishamco_2007@hotmail.com>
2015-08-05 23:13:54 +0300

    Add 'Microsoft.AspNet.Localization' Unit Tests

---
+++ test/Microsoft.AspNet.Localization.Tests/CookieRequestCultureProviderTest.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved. 
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Localization;
+using Microsoft.AspNet.TestHost;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using Xunit;
+
+namespace Microsoft.Framework.Localization.Tests
+{
+    public class CookieRequestCultureProviderTest
+    {
+        [Fact]
+        public async void GetCultureInfoFromPersistentCookie()
+        {
+            using (var server = TestServer.Create(app =>
+            {
+                var options = new RequestLocalizationOptions();
+                var provider = new CookieRequestCultureProvider();
+                provider.CookieName = "Preferences";
+                options.RequestCultureProviders.Insert(0, provider);
+                app.UseRequestLocalization(options);
+                app.Run(context =>
+                {
+                    var requestCultureFeature = context.GetFeature<IRequestCultureFeature>();
+                    var requestCulture = requestCultureFeature.RequestCulture;
+                    Assert.Equal("ar-SA", requestCulture.Culture.Name);
+                    return Task.FromResult(0);
+                });
+            },
+            services =>
+            {
+                services.AddLocalization();
+            }))
+            {
+                var client = server.CreateClient();
+                var culture = new CultureInfo("ar-SA");
+                var requestCulture = new RequestCulture(culture);
+                var value = CookieRequestCultureProvider.MakeCookieValue(requestCulture);
+                client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("Preferences", value).ToString());
+                var response = await client.GetAsync(string.Empty);
+            }
+        }
+
+        [Fact]
+        public async void GetDefaultCultureInfoIfCultureKeysAreMissingOrInvalid()
+        {
+            using (var server = TestServer.Create(app =>
+            {
+                var options = new RequestLocalizationOptions();
+                var provider = new CookieRequestCultureProvider();
+                provider.CookieName = "Preferences";
+                options.RequestCultureProviders.Insert(0, provider);
+                app.UseRequestLocalization(options);
+                app.Run(context =>
+                {
+                    var requestCultureFeature = context.GetFeature<IRequestCultureFeature>();
+                    var requestCulture = requestCultureFeature.RequestCulture;
+                    Assert.Equal(options.DefaultRequestCulture.Culture.Name, requestCulture.Culture.Name);
+                    return Task.FromResult(0);
+                });
+            },
+            services =>
+            {
+                services.AddLocalization();
+            }))
+            {
+                var client = server.CreateClient();
+                client.DefaultRequestHeaders.Add("Cookie", new CookieHeaderValue("Preferences", "uic=ar-SA").ToString());
+                var response = await client.GetAsync(string.Empty);
+            }
+        }
+
+        [Fact]
+        public async void GetDefaultCultureInfoIfCookieIsNotExist()
+        {
+            using (var server = TestServer.Create(app =>
+            {
+                var options = new RequestLocalizationOptions();
+                var provider = new CookieRequestCultureProvider();
+                provider.CookieName = "Preferences";
+                options.RequestCultureProviders.Insert(0, provider);
+                app.UseRequestLocalization(options);
+                app.Run(context =>
+                {
+                    var requestCultureFeature = context.GetFeature<IRequestCultureFeature>();
+                    var requestCulture = requestCultureFeature.RequestCulture;
+                    Assert.Equal(options.DefaultRequestCulture.Culture.Name, requestCulture.Culture.Name);
+                    return Task.FromResult(0);
+                });
+            },
+            services =>
+            {
+                services.AddLocalization();
+            }))
+            {
+                var client = server.CreateClient();
+                var response = await client.GetAsync(string.Empty);
+            }
+        }
+    }
+}
