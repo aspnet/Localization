f62edec55511b6d200ec62374a3d13f3326673e0
Hisham Bin Ateya <hishamco_2007@hotmail.com>
2015-09-22 10:09:41 +0300

    Using '?.' operator to simplify the syntax

--- src/Microsoft.AspNet.Localization/AcceptLanguageHeaderRequestCultureProvider.cs
+++ src/Microsoft.AspNet.Localization/AcceptLanguageHeaderRequestCultureProvider.cs
@@ -57,7 +57,7 @@ namespace Microsoft.AspNet.Localization
 
                         requestCulture = ValidateRequestCulture(requestCulture);
 
-                        if (requestCulture != null && requestCulture.Culture == culture)
+                        if (requestCulture?.Culture == culture)
                         {
                             return Task.FromResult(requestCulture);
                         }
