6b4fbaa5a9a597d5f59bb6abb5bcc278bd5fb81a
Kirthi Krishnamraju <kirthik@microsoft.com>
2015-10-13 15:01:59 -0700

    changes

--- test/Microsoft.AspNet.Localization.FunctionalTests/LocalizationTest.cs
+++ test/Microsoft.AspNet.Localization.FunctionalTests/LocalizationTest.cs
@@ -3,18 +3,21 @@
 
 using System.Threading.Tasks;
 using Microsoft.AspNet.Server.Testing;
+using Microsoft.AspNet.Testing.xunit;
 using Xunit;
 
 namespace Microsoft.AspNet.Localization.FunctionalTests
 {
     public class LocalizationTest
     {
-        [Theory]
-        [InlineData(RuntimeFlavor.Clr, "http://localhost:5070/")]
-        [InlineData(RuntimeFlavor.CoreClr, "http://localhost:5071/")]
-        public Task Localization_ResourcesInFolder_ReturnLocalizedValue(
+        [ConditionalTheory]
+        [OSSkipCondition(OperatingSystems.Linux | OperatingSystems.MacOSX)]
+        [InlineData(RuntimeFlavor.Clr, "http://localhost:5070/", RuntimeArchitecture.x86)]
+        [InlineData(RuntimeFlavor.CoreClr, "http://localhost:5071/", RuntimeArchitecture.x86)]
+        public Task Localization_ResourcesInFolder_ReturnLocalizedValue_Windows(
             RuntimeFlavor runtimeFlavor,
-            string applicationBaseUrl)
+            string applicationBaseUrl,
+            string architecture)
         {
             var testRunner = new TestRunner();
             return testRunner.RunTestAndVerifyResponse(
@@ -25,12 +28,52 @@ namespace Microsoft.AspNet.Localization.FunctionalTests
                 "Bonjour from StartupResourcesInFolder Bonjour from Test in resources folder");
         }
 
-        [Theory]
-        [InlineData(RuntimeFlavor.Clr, "http://localhost:5072/")]
-        [InlineData(RuntimeFlavor.CoreClr, "http://localhost:5073/")]
-        public Task Localization_ResourcesAtRootFolder_ReturnLocalizedValue(
+        [ConditionalTheory]
+        [OSSkipCondition(OperatingSystems.Win7And2008R2)]
+        [FrameworkSkipCondition(RuntimeFrameworks.CLR)]
+        [InlineData(RuntimeFlavor.Mono, "http://localhost:5072/", RuntimeArchitecture.x86)]
+        [InlineData(RuntimeFlavor.CoreClr, "http://localhost:5073/", RuntimeArchitecture.x64)]
+        public Task Localization_ResourcesInFolder_ReturnLocalizedValue_Mono(
             RuntimeFlavor runtimeFlavor,
-            string applicationBaseUrl)
+            string applicationBaseUrl,
+            string architecture)
+        {
+            var testRunner = new TestRunner();
+            return testRunner.RunTestAndVerifyResponse(
+                runtimeFlavor,
+                applicationBaseUrl,
+                "ResourcesInFolder",
+                "fr-FR",
+                "Bonjour from StartupResourcesInFolder Bonjour from Test in resources folder");
+        }
+
+        [ConditionalTheory]
+        [OSSkipCondition(OperatingSystems.Linux | OperatingSystems.MacOSX)]
+        [InlineData(RuntimeFlavor.Clr, "http://localhost:5074/", RuntimeArchitecture.x86)]
+        [InlineData(RuntimeFlavor.CoreClr, "http://localhost:5075/", RuntimeArchitecture.x86)]
+        public Task Localization_ResourcesAtRootFolder_ReturnLocalizedValue_Windows(
+            RuntimeFlavor runtimeFlavor,
+            string applicationBaseUrl,
+            string architecture)
+        {
+            var testRunner = new TestRunner();
+            return testRunner.RunTestAndVerifyResponse(
+                runtimeFlavor,
+                applicationBaseUrl,
+                "ResourcesAtRootFolder",
+                "fr-FR",
+                "Bonjour from StartupResourcesAtRootFolder Bonjour from Test in root folder");
+        }
+
+        [ConditionalTheory]
+        [OSSkipCondition(OperatingSystems.Win7And2008R2)]
+        [FrameworkSkipCondition(RuntimeFrameworks.CLR)]
+        [InlineData(RuntimeFlavor.Mono, "http://localhost:5076/", RuntimeArchitecture.x86)]
+        [InlineData(RuntimeFlavor.CoreClr, "http://localhost:5077/", RuntimeArchitecture.x64)]
+        public Task Localization_ResourcesAtRootFolder_ReturnLocalizedValue_Mono(
+            RuntimeFlavor runtimeFlavor,
+            string applicationBaseUrl,
+            string architecture)
         {
             var testRunner = new TestRunner();
             return testRunner.RunTestAndVerifyResponse(
