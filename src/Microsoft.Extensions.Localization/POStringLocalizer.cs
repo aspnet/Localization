4df771062777425bb05edce513ce2e1d35d9e6b9
Ryan Brandenburg <rybrande@microsoft.com>
2016-10-11 14:45:30 -0700

    PO string localizer

---
+++ src/Microsoft.Extensions.Localization/POStringLocalizer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.Localization
+{
+    public class POStringLocalizer : IStringLocalizer
+    {
+        private readonly POManager _poManager;
+
+        public LocalizedString this[string name]
+        {
+            get
+            {
+                if (name == null)
+                {
+                    throw new ArgumentNullException(nameof(name));
+                }
+
+                var value = GetStringSafely(name, null);
+                return new LocalizedString(name, value ?? name, resourceNotFound: value == null);
+            }
+        }
+
+        public LocalizedString this[string name, params object[] arguments]
+        {
+            get
+            {
+                if (name == null)
+                {
+                    throw new ArgumentNullException(nameof(name));
+                }
+
+                var format = GetStringSafely(name, null);
+                var value = string.Format(format ?? name, arguments);
+                return new LocalizedString(name, value, resourceNotFound: format == null);
+            }
+        }
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IStringLocalizer WithCulture(CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected string GetStringSafely(string name, CultureInfo culture)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            return culture == null ? _poManager.GetString(name) : _poManager.GetString(name, culture);
+        }
+    }
+}
