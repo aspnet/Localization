6b5b8dc96008c63a112f72401181d837b05bb5c8
Ryan Brandenburg <rybrande@microsoft.com>
2016-10-13 16:28:13 -0700

    Minor work

---
+++ src/Microsoft.Extensions.Localization/POManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Microsoft.Extensions.Localization
+{
+    public class POManager
+    {
+        public POManager()
+        {
+
+        }
+
+        public string GetString(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetString(string name, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
